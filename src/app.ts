eab3d212fa7b9ea878577aa5d5d8b3f6c09ead79
Oieusouopi <rafasc866@gmail.com>
2022-10-12 20:13:53 -0300

    configuração de ambiente com eslint

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
 class App {
-    private app: express.Express;
+	private app: express.Express;
 
-    constructor() {
-        this.app = express()
+	constructor() {
+		this.app = express();
 
-        this.app.use(express.json())
+		this.app.use(express.json());
 
-        this.app.get("/", (_req: Request, res: Response) => res.json({
-            message: "Hello World"
-        }))
-    }
+		this.app.get("/", (_req: Request, res: Response) => res.json({
+			message: "Hello World"
+		}));
+	}
 
-    public start(): void {
-        this.app.listen(3333, () => console.log("rodando na porta 3333"))
-    }
+	public start(): void {
+		this.app.listen(3333, () => console.log("rodando na porta 3333"));
+	}
 }
 
 export default App;
