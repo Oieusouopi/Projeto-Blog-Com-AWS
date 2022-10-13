856d89d56f95d8f1cf42bf546b2cae3152346ded
Oieusouopi <rafasc866@gmail.com>
2022-10-12 22:12:55 -0300

    repositorios com as nossas operações dos usuários

---
+++ src/repositories/UserCases.ts
@@ -0,0 +1,8 @@
+import { User } from "../entities/User";
+
+export interface UserCases {
+    create(user: User): Promise<void>,
+    read(name: string): Promise<User>,
+    update(name: string): Promise<User>,
+    delete(name: string): Promise<void>,
+}
