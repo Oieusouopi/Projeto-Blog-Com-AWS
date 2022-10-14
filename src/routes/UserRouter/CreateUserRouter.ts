7a498174161ec6f712e07087ff984fb6428f1ef6
Oieusouopi <rafasc866@gmail.com>
2022-10-14 02:00:13 -0300

    rota para criar o usuário

--- src/routes/UserRouter/CreateUserRouter.ts
+++ src/routes/UserRouter/CreateUserRouter.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { factoryCreateUser } from "../../UserCases/CreateUser";
 
 export class CreateUserRouter {
 	router: Router;
 	constructor() {
 		this.router = Router();
-		this.router.post("/register", (req: Request, res: Response, _next: NextFunction) => {
-			return res.status(201).json();
+		this.router.post("/register", (req: Request, res: Response, next: NextFunction) => {
+			factoryCreateUser().handle(req,res,next);
 		});
 	}
 }
