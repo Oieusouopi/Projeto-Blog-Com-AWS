4e6772c8b2b23393903c76a51f5175fd45d5613d
Oieusouopi <rafasc866@gmail.com>
2022-10-14 02:00:43 -0300

    modificando o nome parametro que o costrutor recebe

--- src/controller/UserController/CreateUserController.ts
+++ src/controller/UserController/CreateUserController.ts
@@ -2,12 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import { ValidationUserBody } from "../../UserCases/CreateUser/ValidationUserBody";
 
 export class CreateUserController {
-	constructor(private createUserService: ValidationUserBody) {}
+	constructor(private validationUserBody: ValidationUserBody) {}
 
 	public handle = async (req: Request, res: Response, next: NextFunction) => {
 		try {
-			const { name, nickName, password, email, admin } = req.body;
-			await this.createUserService.execute({name, nickName, password, email, admin});
+			const { name, nickName, password, email } = req.body;
+			await this.validationUserBody.execute({name, nickName, password, email, admin: false});
 			return res.status(201).json({ message: "Usuário criado com sucesso" }); 
 		} catch (error) {
 			next(error);
