f545d55728e603feda1239c5f7de5ab293d73f2f
Oieusouopi <rafasc866@gmail.com>
2022-10-20 12:35:27 -0300

    Error unit test

---
+++ src/UserCases/Tests/CreateUserTest/ValidationUserBody.test.ts
@@ -0,0 +1,8 @@
+// import { describe } from "mocha";
+// import { CreateUserCase } from "../../CreateUser/CreateUserCase";
+// import { ValidationUserBody } from "../../CreateUser/ValidationUserBody";
+
+// describe("Validation body to creating user", () => {
+// 	it("If all DTO exist", () => {
+//     }
+// });
