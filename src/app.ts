7e39c267ccb3500ab0612e866f15124337d73af9
Oieusouopi <rafasc866@gmail.com>
2022-11-01 17:53:42 -0300

    usando os erros tratados

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import treatedError from "./middlewares/error";
 import router from "./routes/MainRouter";
 class App {
 	private app: express.Express;
@@ -9,6 +10,8 @@ class App {
 		this.app.use(express.json());
 
 		this.app.use(router);
+
+		this.app.use(treatedError);
 
 		this.app.get("/", (_req: Request, res: Response) => res.json({
 			message: "Hello World"
