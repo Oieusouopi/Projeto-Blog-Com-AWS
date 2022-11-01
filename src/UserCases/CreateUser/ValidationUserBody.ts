2d46a3a6fd6bcc1bd2453cf4240e27a4b72e8c75
Oieusouopi <rafasc866@gmail.com>
2022-11-01 16:34:52 -0300

    adicionando validações para senha e email

--- src/UserCases/CreateUser/ValidationUserBody.ts
+++ src/UserCases/CreateUser/ValidationUserBody.ts
@@ -5,29 +5,45 @@ export class ValidationUserBody {
 	constructor(private createUserCase: CreateUserCase) {}
 	
 	public execute = async (userData: ICreateUserDTO) => {
-		const { name, nickName, password, email, admin } = userData;
+		const { name, nickName, password, email } = userData;
 		this.validUserFields(userData);
 		this.validationName(name);
 		this.validationNickName(nickName);
+		this.validationEmail(email);
+		this.validationPassword(password);
 		await this.createUserCase.execute(userData);
 	};
 
 	private validUserFields = (userData: ICreateUserDTO) => {
 		const { name, nickName, password, email, admin } = userData;
-		if (!name && nickName && password && email && admin) {
-			return Error("Some field is empty");
+		if (!name && !nickName && !password && !email && !admin) {
+			return Error("Algum campo está vazio");
 		}
 	};
 
 	private validationName = (name: string) => {
 		if (name.length <= 7) {
-			return Error("name is too short please enter a name longer than 6 characters");
+			return Error("nome muito curto por favor escreva um nome maior que 3 caracteres");
 		}
 	};
 
 	private validationNickName = (nickName: string) => {
-		if (nickName.length <= 3) {
-			return Error("nickName is too short please enter a name longer than 3 characters");
+		if (nickName.length <= 1) {
+			return Error("Apelido muito curto por favor escreva um nome maior que 1 caracteres");
+		}
+	};
+
+	private validationEmail = (email: string) => {
+		const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+		if (regexEmail.test(email) === false) {
+			return Error("Email incorreto por favor informe um email correto");
+		}
+	};
+
+	private validationPassword = (password: string) => {
+		const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{6,}$/;
+		if (regexPassword.test(password) === false) {
+			return Error("Sua senha deve conter mais que 8 digitos uma letra maiuscula e minuscula e um caracter especial");
 		}
 	};
 }
