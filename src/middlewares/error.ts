3ae29af32a07dd7378a0aed11f06f97eded75b11
Oieusouopi <rafasc866@gmail.com>
2022-11-01 17:53:56 -0300

    error tratado

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,15 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, Request, Response } from "express";
+import { errorCatalog, ErrorTypes } from "../UserCases/utils/ErrorCatalog";
 
 const treatedError: ErrorRequestHandler = (
-	err: Error, _req: Request, res: Response, next: NextFunction) => {
+	err: Error, _req: Request, res: Response) => {
+	const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+	const mappedError = errorCatalog[messageAsErrorType];
+	if (mappedError) {
+		const { httpStatus, error } = mappedError;
+		return res.status(httpStatus).json({ error });
+	}
+	return res.status(500).json({ message: "Servidor Error" });    
 };
+
+export default treatedError;
